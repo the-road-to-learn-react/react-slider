55e70dbed07d0bb79048ad62803c819e655d6f44
Robin Wieruch <wrobin@gmx.net>
2019-12-03 10:02:18 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# react-slider
+
+[![Build Status](https://travis-ci.org/the-road-to-learn-react/react-slider.svg?branch=master)](https://travis-ci.org/the-road-to-learn-react/react-slider) [![Slack](https://slack-the-road-to-learn-react.wieruch.com/badge.svg)](https://slack-the-road-to-learn-react.wieruch.com/) [![Greenkeeper badge](https://badges.greenkeeper.io/the-road-to-learn-react/react-slider.svg)](https://greenkeeper.io/)
+
+Showcasing how a React Slider could be implemented in React. [Read more about it.](https://www.robinwieruch.de/react-slider/)
+
+[![Edit react-slider](https://codesandbox.io/static/img/play-codesandbox.svg)](https://codesandbox.io/s/github/the-road-to-learn-react/react-slider/tree/master/?fontsize=14)
+
+## Installation
+
+- `git clone git@github.com:the-road-to-learn-react/react-slider.git`
+- cd react-slider
+- npm install
+- npm start
+- visit `http://localhost:3000`
---
+++ src/App.js
@@ -0,0 +1,117 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const SliderHeader = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const StyledSlider = styled.div`
+  border-radius: 3px;
+  background: #dddddd;
+  margin: 5px;
+  height: 15px;
+`;
+
+const StyledThumb = styled.div`
+  width: 10px;
+  height: 25px;
+  border-radius: 3px;
+  position: relative;
+  top: -5px;
+  opacity: 0.5;
+  background: #823eb7;
+  cursor: pointer;
+`;
+
+const getPercentage = (current, max) => (100 * current) / max;
+
+const getValue = (percentage, max) => (max / 100) * percentage;
+
+const getLeft = percentage => `calc(${percentage}% - 5px)`;
+
+const Slider = ({
+  initial,
+  max,
+  formatFn = number => number.toFixed(0),
+  onChange,
+}) => {
+  const initialPercentage = getPercentage(initial, max);
+
+  const sliderRef = React.useRef();
+  const thumbRef = React.useRef();
+  const currentRef = React.useRef(initial);
+
+  const diff = React.useRef();
+
+  const handleMouseMove = event => {
+    let newX =
+      event.clientX -
+      diff.current -
+      sliderRef.current.getBoundingClientRect().left;
+
+    const end =
+      sliderRef.current.offsetWidth - thumbRef.current.offsetWidth;
+
+    const start = 0;
+
+    if (newX < start) {
+      newX = 0;
+    }
+
+    if (newX > end) {
+      newX = end;
+    }
+
+    const newPercentage = getPercentage(newX, end);
+    const newValue = getValue(newPercentage, max);
+
+    thumbRef.current.style.left = getLeft(newPercentage);
+    currentRef.current.textContent = formatFn(newValue);
+
+    onChange(newValue);
+  };
+
+  const handleMouseUp = () => {
+    document.removeEventListener('mouseup', handleMouseUp);
+    document.removeEventListener('mousemove', handleMouseMove);
+  };
+
+  const handleMouseDown = event => {
+    diff.current =
+      event.clientX - thumbRef.current.getBoundingClientRect().left;
+
+    document.addEventListener('mousemove', handleMouseMove);
+    document.addEventListener('mouseup', handleMouseUp);
+  };
+
+  return (
+    <>
+      <SliderHeader>
+        <strong ref={currentRef}>{formatFn(initial)}</strong>
+        &nbsp;/&nbsp;
+        {max}
+      </SliderHeader>
+      <StyledSlider ref={sliderRef}>
+        <StyledThumb
+          style={{ left: getLeft(initialPercentage) }}
+          ref={thumbRef}
+          onMouseDown={handleMouseDown}
+        />
+      </StyledSlider>
+    </>
+  );
+};
+
+const App = () => (
+  <div>
+    <Slider
+      initial={10}
+      max={25}
+      formatFn={number => number.toFixed(2)}
+      onChange={value => console.log(value)}
+    />
+  </div>
+);
+
+export default App;
